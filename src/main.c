e2698b29df5b7fab83a7dd1edc83b8a7a36f9ba4
Shivam <135974981+ShivamBhuskute@users.noreply.github.com>
2025-10-18 11:53:44 +0530

    instruction size calculation(Supports only 16 bit operations for now) (#4)
    * removes white spaces
    
    * calculates instruction sizes correctly
    
    * formatted O/P
    
    * changed assembly code

--- src/main.c
+++ src/main.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <utils/instruction_size.h>
-
 #include "parser/parser.h"
 #include "symbol_table/symbol_table.h"
 
@@ -24,33 +23,31 @@ int main(int argc, char *argv[]) {
     char line[256];
     int LC = 0;
 
-    // Pass 1
+    // Pass 1: display code with LC
     while (fgets(line, sizeof(line), f)) {
-        printf("Line: %s", line); // debug
         ParsedLine *pl = parse_line(line);
-        
+
+        // Display current LC address and code
+        printf("%04X:    %s", LC, line);
+
         if (pl->label) {
-            printf("  Found label: %s at LC=%d\n", pl->label, LC);
             add_symbol(&table, pl->label, LC);
         }
-    
+
         if (pl->mnemonic) {
             int size = get_instruction_size(pl);
-            printf("  Found instruction: %s (size %d)\n", pl->mnemonic, size);
             LC += size;
         }
-    
+
         free_parsed_line(pl);
     }
 
-
     fclose(f);
 
-    printf("\nSymbol Table:\n ");
+    printf("\n\nSymbol Table:\n");
     for (int i = 0; i < table.count; i++) {
         printf("%s -> %d\n", table.symbols[i].name, table.symbols[i].address);
     }
-
     free_symbol_table(&table);
 
     return 0;
